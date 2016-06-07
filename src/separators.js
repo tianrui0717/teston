42489bba8fce64621dc8b13be893ac77194c60b3
tianrui0717 <1156184421@qq.com>
2016-06-07 13:08:36 +0800

    testgit2

--- src/separators.js
+++ src/separators.js
@@ -1,10 +1,9 @@
 'use strict';
 
 function thousands_separators(num) {
-  if(num<100)
-	  alert(num)
-  else if
-  	  alert
+	 var num_parts = num.toString().split(".");
+	 num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
+	 return num_parts.join("."); 
 }
 
 module.exports = thousands_separators;
